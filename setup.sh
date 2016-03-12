e727c58f937e9aec605782d44e47e7637ae723b4
Pete <epwalsh10@gmail.com>
2016-03-11 21:23:09 -0600

    create setup file

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Careful! Running this script will overwrite all the files below in your
+# home directory.
+
+DOTFILES=".Rprofile
+.bash_profile
+.bashrc
+.tmux-osx.conf
+.tmux.conf
+.vimrc"
+
+for f in $DOTFILES
+do
+    if [ -e $HOME/$f ]
+    then
+        rm $HOME/$f
+    fi
+    ln -s $f $HOME/$f
+done
