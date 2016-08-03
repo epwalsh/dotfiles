464b6c95fbe6433556dfdc57e9e9e840503f007b
Pete <epwalsh10@gmail.com>
2016-08-03 15:03:04 -0500

    updates

--- bin/battery.sh
+++ bin/battery.sh
@@ -1,37 +1,20 @@
 #!/bin/bash
-# Source: modified from github.com/AaronLasseigne/tmux_battery_charge_indicator 
 
-UNCHARGE_SYMBOL='▯'
-CHARGE_SYMBOL='▮'
+HEART='❤'
 
-if [[ `uname` == 'Linux' ]]; then
-  current_charge=$(cat /proc/acpi/battery/BAT1/state | grep 'remaining capacity' | awk '{print $3}')
-  total_charge=$(cat /proc/acpi/battery/BAT1/info | grep 'last full capacity' | awk '{print $4}')
-else
-  battery_info=`ioreg -rc AppleSmartBattery`
-  current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
-  total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
-fi
+battery_info=`pmset -g batt`
+current_charge=$(echo $battery_info | grep -o '[0-9]\+%' | awk '{sub (/%/, "", $1); print $1}')
 
-charged_slots=$(echo "(($current_charge/$total_charge)*10)+1" | bc -l | cut -d '.' -f 1)
-if [[ $charged_slots -gt 10 ]]; then
-  charged_slots=10
-fi
-
-if [[ $charged_slots -lt 3 ]]; then
+if [[ $current_charge -lt 30 ]]; then
     echo -n '#[fg=colour41]'
-elif [[ $charged_slots -lt 5 ]]; then
+elif [[ $current_charge -lt 50 ]]; then
     echo -n '#[fg=colour42]'
-elif [[ $charged_slots -lt 7 ]]; then
+elif [[ $current_charge -lt 70 ]]; then
     echo -n '#[fg=colour43]'
-elif [[ $charged_slots -lt 9 ]]; then
+elif [[ $current_charge -lt 90 ]]; then
     echo -n '#[fg=colour44]'
 else
     echo -n '#[fg=colour45]'
 fi
 
-for i in `seq 1 $charged_slots`; do echo -n "$CHARGE_SYMBOL"; done
-
-if [[ $charged_slots -lt 10 ]]; then
-  for i in `seq 1 $(echo "10-$charged_slots" | bc)`; do echo -n "$UNCHARGE_SYMBOL"; done
-fi
+echo -n "$HEART $current_charge%"
