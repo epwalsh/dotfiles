74964ba489b37932217d8ca12e5cfbac9c192479
epwalsh <epwalsh10@gmail.com>
2018-08-23 15:16:45 -0700

    idk

--- prepare_all.sh
+++ prepare_all.sh
@@ -14,6 +14,13 @@ rm -rf fonts/
 cd ~/dotfiles
 
 # Install vcprompt.
-mkdir -p ~/bin
-curl -sL https://github.com/djl/vcprompt/raw/master/bin/vcprompt > ~/bin/vcprompt
-chmod 755 ~/bin/vcprompt
+mkdir -p $HOME/bin
+curl -sL https://github.com/djl/vcprompt/raw/master/bin/vcprompt > $HOME/bin/vcprompt
+chmod 755 $HOME/bin/vcprompt
+
+# Setup python virtualenv.
+export WORKON_HOME=$HOME/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+mkvirtualenv py3.6 -p $(which python3.6)
+workon py3.6
+pip install -r requirements.txt
