4e116703b6e068fcf1547ac6ed355e0dccbef7b3
epwalsh <epwalsh10@gmail.com>
2018-08-23 15:28:22 -0700

    fix

--- prepare_linux.sh
+++ prepare_linux.sh
@@ -3,11 +3,10 @@ apt-get update
 apt-get install -y \
     build-essential \
     curl \
-    git \
     tmux \
     python-setuptools
 
 # Python 3.6.
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get update
-sudo apt-get install python3.6
+add-apt-repository ppa:deadsnakes/ppa
+apt-get update
+apt-get install python3.6
