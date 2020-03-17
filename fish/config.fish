1a00328a9e1fec7d065c5615fcb151a0de8d1db7
epwalsh <epwalsh10@gmail.com>
2020-03-17 12:29:22 -0700

    set vi bindings for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,9 @@
 eval (python -m virtualfish auto_activation global_requirements)
 
+set -gx PIPENV_IGNORE_VIRTUALENVS 1
+
+set -gx WORKON_HOME $HOME/.virtualenvs
+
 # Default virtual env
 if not set -q VIRTUAL_ENV
     vf activate py3.6
@@ -11,6 +15,5 @@ set -gx PATH ~/.cargo/bin/ $PATH
 # Set default editor
 set -gx EDITOR nvim
 
-set -gx PIPENV_IGNORE_VIRTUALENVS 1
-
-set -gx WORKON_HOME $HOME/.virtualenvs
+# Vi bindings
+fish_vi_key_bindings
