5d8378f48f7b9c11836ca025f62c562b6f98947e
epwalsh <epwalsh10@gmail.com>
2020-04-07 15:57:23 -0700

    fixes

--- prepare_all.sh
+++ prepare_all.sh
@@ -1,8 +1,8 @@
 # Base python dependencies.
-pip2 install --user --upgrade pip setuptools wheel
-pip install --user --upgrade pip setuptools wheel
-pip2 install --user -r requirements.txt
-pip install --user -r requirements.txt
+# pip2 install --user --upgrade pip setuptools wheel
+# pip3 install --user --upgrade pip setuptools wheel
+# pip2 install --user -r requirements.txt
+# pip3 install --user -r requirements.txt
 
 # Powerline fonts.
 cd ~/Downloads
@@ -14,14 +14,14 @@ rm -rf fonts/
 cd ~/dotfiles
 
 # Install vcprompt.
-mkdir -p $HOME/bin
-curl -sL https://github.com/djl/vcprompt/raw/master/bin/vcprompt > $HOME/bin/vcprompt
-chmod 755 $HOME/bin/vcprompt
+# mkdir -p $HOME/bin
+# curl -sL https://github.com/djl/vcprompt/raw/master/bin/vcprompt > $HOME/bin/vcprompt
+# chmod 755 $HOME/bin/vcprompt
 
 # Setup python virtualenv.
-export WORKON_HOME=$HOME/.virtualenvs
-export VIRTUALENVWRAPPER_PYTHON=$(which python3)
-source /usr/local/bin/virtualenvwrapper.sh
-mkvirtualenv py3 -p $(which python3)
-workon py3
-pip install -r requirements.txt
+# export WORKON_HOME=$HOME/.virtualenvs
+# export VIRTUALENVWRAPPER_PYTHON=$(which python3)
+# source /usr/local/bin/virtualenvwrapper.sh
+# mkvirtualenv py3 -p $(which python3)
+# workon py3
+# pip install -r requirements.txt
