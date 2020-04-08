ba6ab329383b1bed2d7d22eeb0d7e752e871fdf4
Evan Pete Walsh <epwalsh10@gmail.com>
2020-04-07 17:51:05 -0700

    Vim plug (#4)
    * switch to vim-plug
    
    * update install script

--- install_configs.sh
+++ install_configs.sh
@@ -34,5 +34,5 @@ ln -sf ~/dotfiles/.pydocstyle ~/.config/.pydocstyle
 
 # Neovim plugins.
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
-nvim +PluginInstall +qall
+nvim +PlugInstall +qall
 nvim +UpdateRemotePlugins +qall
