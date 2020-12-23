10ebd8fae9fd9a290ad36d9238880b989868fbba
epwalsh <epwalsh10@gmail.com>
2020-12-23 10:02:07 -0800

    updates

--- fish/functions/Gdelete-branch.fish
+++ fish/functions/Gdelete-branch.fish
@@ -1,9 +1,10 @@
 function Gdelete-branch
     set current_branch (git rev-parse --abbrev-ref HEAD)
     set has_upstream_remote (git remote -v | grep upstream | wc -l)
-    git checkout master
+    set default_branch (git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+    git checkout $default_branch
     if test $has_upstream_remote -ne 0
-        git pull --rebase upstream master
+        git pull --rebase upstream $default_branch
     else
         git pull
     end
