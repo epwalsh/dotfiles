efe483b0b2618a7b61ef40b509d7d45f87ff4bbe
epwalsh <petew@allenai.org>
2021-07-16 17:40:03 +0000

    fix auto_activation.fish

--- fish/auto_activation.fish
+++ fish/auto_activation.fish
@@ -2,7 +2,7 @@
 # Automatic Python virtual environment activation.
 ################################################################
 
-if set -q VENV_ACTIVATOR
+if not set -q VENV_ACTIVATOR
     if type -q vf
         set -g VENV_ACTIVATOR vf
     else if type -q conda
@@ -10,7 +10,7 @@ if set -q VENV_ACTIVATOR
     end
 end
 
-if set -q VENV_ACTIVATION_FILE
+if not set -q VENV_ACTIVATION_FILE
     set -g VENV_ACTIVATION_FILE .venv
 end
 
@@ -19,7 +19,7 @@ function __venv_support_auto_activate --on-variable PWD
         return
     end
 
-    if set -q VENV_ACTIVATOR
+    if not set -q VENV_ACTIVATOR
         return
     end
 
