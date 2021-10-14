674b7caad68c9125e6e7ace4f00a4b8bd9091e18
epwalsh <epwalsh@structurely.com>
2021-10-14 15:05:37 +0000

    updates

--- install_configs.sh
+++ install_configs.sh
@@ -3,11 +3,6 @@ shopt -s dotglob
 
 mkdir -p ~/.config
 
-for filename in ~/dotfiles/bash/*; do
-    filebase=`basename $filename`
-    ln -sf $filename ~/$filebase
-done
-
 for filename in ~/dotfiles/tmux/.*; do
     filebase=`basename $filename`
     ln -sf $filename ~/$filebase
@@ -31,8 +26,8 @@ ln -sf ~/dotfiles/alacritty.yml ~/.config/alacritty/
 ln -sf ~/dotfiles/starship.toml ~/.config/starship.toml
 
 # Python linting configs.
-ln -sf ~/dotfiles/.pylintrc ~/.config/.pylintrc
-ln -sf ~/dotfiles/.pydocstyle ~/.config/.pydocstyle
+#ln -sf ~/dotfiles/.pylintrc ~/.config/.pylintrc
+#ln -sf ~/dotfiles/.pydocstyle ~/.config/.pydocstyle
 
 # Neovim plugins.
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
