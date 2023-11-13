b5df0183d55ede7fd0f020f02f8de9fb7d2b4a92
epwalsh <petew@allenai.org>
2023-11-13 11:08:42 -0800

    Integrate AsyncRun with fugitive

--- neovim/lua/plugins/util.lua
+++ neovim/lua/plugins/util.lua
@@ -36,9 +36,13 @@ return {
   {
     "skywind3000/asyncrun.vim",
     lazy = true,
-    cmd = { "AsyncRun", "AsyncStop", "AsyncReset" },
+    cmd = { "AsyncRun", "AsyncStop", "AsyncReset", "Gpush", "Gfetch" },
     init = function()
       vim.g.asyncrun_mode = "term"
+      vim.cmd [[
+      command! -bang -bar -nargs=* Gpush execute 'AsyncRun<bang> -cwd=' . fnameescape(FugitiveGitDir()) 'git push' <q-args>
+      command! -bang -bar -nargs=* Gfetch execute 'AsyncRun<bang> -cwd=' . fnameescape(FugitiveGitDir()) 'git fetch' <q-args>
+      ]]
     end,
   },
 }
