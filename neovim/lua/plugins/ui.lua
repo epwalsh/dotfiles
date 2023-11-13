82161063e7238b50130fd8345dbca5cdab94ec33
epwalsh <petew@allenai.org>
2023-11-13 10:22:09 -0800

    add leap

--- neovim/lua/plugins/ui.lua
+++ neovim/lua/plugins/ui.lua
@@ -249,4 +249,13 @@ return {
       }
     end,
   },
+
+  {
+    "ggandor/leap.nvim",
+    lazy = true,
+    event = { "BufEnter" },
+    config = function()
+      require("leap").add_default_mappings()
+    end,
+  },
 }
