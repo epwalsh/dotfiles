e79a50e1359fea50b72b07547124fe23037656ab
epwalsh <petew@allenai.org>
2024-01-11 16:23:26 -0800

    updates

--- neovim/lua/plugins/mappings.lua
+++ neovim/lua/plugins/mappings.lua
@@ -2,7 +2,12 @@ return {
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+      disable = {
+        -- disable for vim ft so it's disabled for the command buffer ('q:')
+        filetypes = { "vim" },
+      },
+    },
     init = function()
       vim.o.timeout = true
       vim.o.timeoutlen = 300
