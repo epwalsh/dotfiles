304e754f4c2879083f0d81574690f23106f2f9e9
epwalsh <petew@allenai.org>
2024-01-24 13:28:52 -0800

    use 2 spaces again for markdown indent

--- neovim/after/ftplugin/markdown.lua
+++ neovim/after/ftplugin/markdown.lua
@@ -3,8 +3,8 @@
 local paste_img = require("core.img_paste").paste_img
 
 vim.g.markdown_fenced_languages = { "html", "python", "bash=sh", "rust" }
-vim.opt_local.shiftwidth = 4
-vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
 vim.opt_local.expandtab = true
 vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
