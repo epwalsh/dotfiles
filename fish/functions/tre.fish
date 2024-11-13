5322714fda7222e2e3780d354c30a879e63ebc22
epwalsh <petew@allenai.org>
2024-11-13 09:21:36 -0800

    updates

--- fish/functions/tre.fish
+++ fish/functions/tre.fish
@@ -6,7 +6,6 @@ function tre
     end
     exa -T \
         --all \
-        --git-ignore \
         --ignore-glob '**/.git|**/.mypy_cache|**/__pycache__|**/*.egg-info|**/.pytest_cache' \
         --icons \
         --color always \
