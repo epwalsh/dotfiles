bbe9b256039afd05291ce655b7c2d6e1b603dce4
epwalsh <petew@allenai.org>
2025-04-23 16:23:34 -0700

    Add Gcommit command

--- neovim/lua/core/commands.lua
+++ neovim/lua/core/commands.lua
@@ -66,6 +66,14 @@ vim.api.nvim_create_user_command("Gbranch", function(_)
   vim.cmd "Telescope git_branches"
 end, { nargs = 0 })
 
+vim.api.nvim_create_user_command("Gcommit", function(data)
+  local commit_msg = data.args
+  vim.cmd "Git add -A"
+  vim.cmd(string.format('Git commit -m "%s"', commit_msg))
+  vim.cmd "Git push"
+  require("gitsigns").refresh()
+end, { nargs = 1 })
+
 ------------------------
 -- Obsidian commands. --
 ------------------------
