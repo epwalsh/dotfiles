b62946732b9c516ddcc6df08b573315cd93a510b
epwalsh <petew@allenai.org>
2026-01-22 08:08:32 -0800

    updates

--- neovim/after/ftplugin/markdown.lua
+++ neovim/after/ftplugin/markdown.lua
@@ -22,3 +22,9 @@ vim.opt_local.foldlevel = 99
 
 -- Start an ipython session
 vim.keymap.set("n", "<leader>s", ":belowright 10split<cr>:terminal ipython<cr>i", { silent = true, buffer = true })
+
+-- Add useful markdown-specific mappings to vim-surround
+-- See: https://github.com/tpope/vim-surround/issues/15#issuecomment-2209423548
+--
+-- (e)phmasis (bold) with '**'. E.g. with visual selection, type 'Se'.
+vim.cmd 'let b:surround_{char2nr("e")} = "**\r**"'
