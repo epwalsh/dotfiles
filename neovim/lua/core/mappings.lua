96c9a0e930d8aacc779ab61bb918770f4428e45c
epwalsh <petew@allenai.org>
2026-01-26 10:07:10 -0800

    fix copy buffer path

--- neovim/lua/core/mappings.lua
+++ neovim/lua/core/mappings.lua
@@ -66,7 +66,7 @@ vim.keymap.set("t", "<esc>", "<C-\\><C-n>", { silent = true })
 -- Toggle buffer options.
 wk.add {
   { "<leader>b", group = "Buffer options" },
-  { "<leader>bc", "<cmd>call setreg('*', expand('%'))<cr>", desc = "Copy the buffer's path" },
+  { "<leader>bc", "<cmd>call setreg('*', fnamemodify(expand('%:p'), ':.'))<cr>", desc = "Copy the buffer's path" },
   { "<leader>bf", "<cmd>set foldenable!<cr>", desc = "Toggle folding" },
   {
     "<leader>bg",
