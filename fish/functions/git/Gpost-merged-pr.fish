ee92e556622af5017d4c1cf670782721e546c1eb
epwalsh <petew@allenai.org>
2026-02-04 16:08:54 -0800

    improve Gpost-merged-pr function

--- fish/functions/git/Gpost-merged-pr.fish
+++ fish/functions/git/Gpost-merged-pr.fish
@@ -3,12 +3,18 @@ function Gpost-merged-pr
     set -l default_branch (git_get_default_branch)
 
     git checkout $default_branch || return 1
+    git pull || return 1
+    git branch -D $current_branch
 
     set -l has_upstream_remote (git remote -v | grep upstream | wc -l)
     if test $has_upstream_remote -ne 0
-        git pull --rebase upstream $default_branch || return 1
-    else
-        git pull || return 1
+        read -l -P 'Do you want to rebase with upstream remote? [y/n] ' confirm
+        switch $confirm
+            case Y y ''
+                git pull --rebase upstream $default_branch || return 1
+                return 0
+            case N n '*'
+                return 0
+        end
     end
-    git branch -D $current_branch
 end
