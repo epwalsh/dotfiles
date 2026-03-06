3f6b6f31d00efd11edbaa8b5e216d2fc50362251
epwalsh <petew@allenai.org>
2026-03-06 09:53:39 -0800

    updates

---
+++ tmux/main_session.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+session_name=main
+if ! tmux has-session -t $session_name 2>/dev/null; then
+    # First window in ~/dotfiles/.
+    tmux new-session -d -s $session_name -n dotfiles -c ~/dotfiles
+    # Target format: session:window.pane
+    tmux split-window -h -t "$session_name:1" -c ~/dotfiles
+    tmux send-keys -t "$session_name:1.2" 'claude' Enter
+    tmux select-pane -t "$session_name:1.1"
+
+    # Second window in ~/github.com/
+    tmux new-window -t $session_name:2 -c ~/github.com
+fi
+tmux attach-session -t $session_name
