a182ab33dc9f744b95b71ba5ecc0d6e7815bbf75
epwalsh <petew@allenai.org>
2026-03-11 12:19:34 -0700

    Handle uv with auto activation

--- fish/conf.d/z_auto_activation.fish
+++ fish/conf.d/z_auto_activation.fish
@@ -2,35 +2,27 @@
 # Automatic Python virtual environment activation.
 ################################################################
 
-if test -d $HOME/miniconda3/bin
-    eval $HOME/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-    set -gx DEFAULT_VIRTUAL_ENV base
-else
-    set -gx DEFAULT_VIRTUAL_ENV base
-end
-
-if not set -q VENV_ACTIVATOR
-    if type -q vf
-        set -g VENV_ACTIVATOR vf
-    else if type -q conda
-        set -g VENV_ACTIVATOR conda
-    end
-end
+# set -gx DEFAULT_VIRTUAL_ENV base
 
 if not set -q VENV_ACTIVATION_FILE
     set -g VENV_ACTIVATION_FILE .venv
 end
 
 function __venv_activate
-    $VENV_ACTIVATOR activate $argv[1] &> /dev/null
-    set -g current_virtual_env $argv[1]
-    if not set -q VIRTUAL_ENV
-        set -g VIRTUAL_ENV (dirname (dirname (which python)))
+    if test -d $argv[1]
+        # If the virtual env is a directory, activate it directly.
+        source $argv[1]/bin/activate.fish &> /dev/null || return 1
+        echo "Activated virtual environment at $argv[1]"
+    else
+        # Otherwise assume it's managed by virtualfish.
+        vf activate $argv[1] &> /dev/null || return 1
+        echo "Activated virtualfish environment $argv[1]"
     end
-end
 
-if begin; set -q VENV_ACTIVATOR; and not set -q current_virtual_env; and set -q DEFAULT_VIRTUAL_ENV; end
-    __venv_activate $DEFAULT_VIRTUAL_ENV
+    set -gx current_virtual_env $argv[1]
+    if not set -q VIRTUAL_ENV
+        set -gx VIRTUAL_ENV (dirname (dirname (which python)))
+    end
 end
 
 function __venv_support_auto_activate --on-variable PWD
@@ -38,31 +30,41 @@ function __venv_support_auto_activate --on-variable PWD
         return
     end
 
-    if not set -q VENV_ACTIVATOR
-        return
-    end
-
-    # find an auto-activation file
+    # Find an auto-activation file
     set -l activation_root $PWD
     set -l new_virtualenv_name ""
     while test $activation_root != ""
         if test -f "$activation_root/$VENV_ACTIVATION_FILE"
             set new_virtualenv_name (command cat "$activation_root/$VENV_ACTIVATION_FILE")
             break
+        else if test -d "$activation_root/.venv"
+            set new_virtualenv_name "$activation_root/.venv"
+            break
         end
-        # this strips the last path component from the path.
+        # Strip the last path component from the path
         set activation_root (echo $activation_root | sed 's|/[^/]*$||')
     end
 
-    if test $new_virtualenv_name != ""
-        # if the virtualenv in the file is different, switch to it
-        if begin; not set -q current_virtual_env; or test $new_virtualenv_name != (basename $current_virtual_env); end
+    if test $new_virtualenv_name != ""; 
+        if begin; not set -q current_virtual_env; or test $new_virtualenv_name != $current_virtual_env; end
+            # Deactivate the current virtual environment if there is one
+            if set -q current_virtual_env
+                if test -f $current_virtual_env/bin/deactivate.fish
+                    source $current_virtual_env/bin/deactivate.fish &> /dev/null
+                    and echo "Deactivated virtual environment $current_virtual_env"
+                else
+                    vf deactivate &> /dev/null
+                    and echo "Deactivated virtualfish environment $current_virtual_env"
+                end
+            end
+
+            # Activate the new one
             __venv_activate $new_virtualenv_name
         end
-    else if begin; not set -q current_virtual_env; and set -q DEFAULT_VIRTUAL_ENV; end
-        __venv_activate $DEFAULT_VIRTUAL_ENV
+    # else if begin; not set -q current_virtual_env; and set -q DEFAULT_VIRTUAL_ENV; end
+    #     __venv_activate $DEFAULT_VIRTUAL_ENV
     end
 end
 
-#automatically activate if started in a directory with a virtualenv in it
+# Automatically activate if started in a directory with a virtual env in it
 __venv_support_auto_activate
